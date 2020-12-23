94a03a4064fa4d1b34f84f78a60a9c540e5b5dbf
Riaz Uddin <riaz.uddin@personalcapital.com>
2020-12-23 15:16:19 -0600

    missing files

--- react-microservice/react-webapp/src/admin/Products.tsx
+++ react-microservice/react-webapp/src/admin/Products.tsx
@@ -1,14 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Wrapper from './Wrapper'
+import {Product} from '../interfaces/product'
 
 const Products = () => {
-
+    const [products, setProducts] = useState([]);
     useEffect(() => {
         (
             async () => {
-                const response = await fetch("http://localhost:8000/api/products")
-                .then(res => res.ok? res : Promise.reject(res))
-                .then(res => console.log(res.json()))
+                //for some reason http://localhost:8000/api/products is failing
+                const response = await fetch('http://jsonplaceholder.typicode.com/users')
+                .then(res => res.json())
+                .then((result) => {
+                        setProducts(result);
+                    },
+                    (error) => {
+                        console.log("Error products --> ", error.message);
+                    })
+                //const data = await response.json();
+//                 console.log(response)
             }
         )();
     }, []);
@@ -21,21 +30,23 @@ const Products = () => {
                   <table className="table table-striped table-sm">
                     <thead>
                       <tr>
+                        <th>Title</th>
+                        <th>Image</th>
+                        <th>Likes</th>
                         <th>#</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
-                        <th>Header</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>1,003</td>
-                        <td>Integer</td>
-                        <td>nec</td>
-                        <td>odio</td>
-                        <td>Praesent</td>
-                      </tr>
+                        {products.map(
+                        (p: Product) => {
+                          return (<tr key={p.id}>
+                                <td>{p.name}</td>
+                                <td><img src={p.username} height="180" /></td>
+                                <td>{p.email}</td>
+                                <td>{p.id}</td>
+                              </tr>
+                              )
+                        })}
                     </tbody>
                   </table>
                 </div>
